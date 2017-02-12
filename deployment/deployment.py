246911465032efbbcd0567c5f549d8e816f1d6d2
TylerDixon <TylerD1242@gmail.com>
2017-02-11 22:07:37 -0500

    Adding deployment to API Gateway

--- deployment/deployment.py
+++ deployment/deployment.py
@@ -25,38 +25,11 @@ def deploy_stack(config):
     try:
         print 'Uploading functions'
         upload_lambda_functions(deployment_bucket, 'store_handler')
+        stack_name = config['deployment_name'] or 'daily-journal-deployment'
+        deploy_cloud_formation(config['api_gateway_identifier'], stack_name, cloudformation_client, deployment_bucket.name)
+        apigateway_client = api_session.client('apigateway')
 
-        deployment_name = config['deployment_name'] or 'daily-journal-deployment'
-        should_update = True
-        stack_waiter = 'stack_update_complete'
-        try:
-            cloudformation_client.describe_stacks(StackName=deployment_name)
-        except botocore.exceptions.ClientError:
-            should_update = False
-            stack_waiter = 'stack_create_complete'
-
-        print 'Loading configuration'
-        with open(os.path.join(os.getcwd(), os.path.dirname(__file__), 'cloud_formation.json')) as f:
-            cloud_formation_settings = json.load(f)
-            cloud_formation_settings['Resources']['StoreLambda']['Properties']['Code']['S3Bucket'] = deployment_bucket.name
-
-            if should_update:
-                print 'Updating stack'
-                cloudformation_client.update_stack(StackName=deployment_name,
-                                                   Capabilities=['CAPABILITY_NAMED_IAM'],
-                                                   TemplateBody=json.dumps(cloud_formation_settings))
-            else:
-                print 'Creating stack'
-                cloudformation_client.create_stack(StackName=deployment_name,
-                                                   Capabilities=['CAPABILITY_NAMED_IAM'],
-                                                   TemplateBody=json.dumps(cloud_formation_settings))
-        waiter = cloudformation_client.get_waiter(stack_waiter)
-        print 'Waiting for stack {0} to be {1}..'.format(deployment_name, 'update' if should_update else 'create')
-        try:
-            waiter.wait(StackName=deployment_name)
-        except botocore.exceptions.ClientError as err:
-            print 'Failed to deploy stack {}'.format(deployment_name)
-            print err
+        upload_journal_views(apigateway_client, config['api_gateway_identifier'], region)
     finally:
         print 'Deleting s3 objects'
         deployment_bucket.delete_objects(Delete={
@@ -77,3 +50,50 @@ def upload_lambda_functions(bucket, function_name):
     archive_loc = archive_function(temp_zip_dir, function_name)
     bucket.upload_file(archive_loc, function_name + '.zip')
     shutil.rmtree(temp_zip_dir)
+
+def deploy_cloud_formation(api_gateway_identifier, stack_name, cloudformation_client, deployment_bucket_name):
+    should_update = True
+    stack_waiter = 'stack_update_complete'
+    try:
+        cloudformation_client.describe_stacks(StackName=stack_name)
+    except botocore.exceptions.ClientError:
+        should_update = False
+        stack_waiter = 'stack_create_complete'
+
+    print 'Loading configuration'
+    with open(os.path.join(os.getcwd(), os.path.dirname(__file__), 'cloud_formation.json')) as f:
+        cloud_formation_settings = json.load(f)
+        cloud_formation_settings['Resources']['StoreLambda']['Properties']['Code']['S3Bucket'] = deployment_bucket_name
+        cloud_formation_settings['Resources']['DjRestApi']['Properties']['Name'] = api_gateway_identifier
+
+        if should_update:
+            print 'Updating stack'
+            cloudformation_client.update_stack(StackName=stack_name,
+                                               Capabilities=['CAPABILITY_NAMED_IAM'],
+                                               TemplateBody=json.dumps(cloud_formation_settings))
+        else:
+            print 'Creating stack'
+            cloudformation_client.create_stack(StackName=stack_name,
+                                               Capabilities=['CAPABILITY_NAMED_IAM'],
+                                               TemplateBody=json.dumps(cloud_formation_settings))
+    waiter = cloudformation_client.get_waiter(stack_waiter)
+    print 'Waiting for stack {0} to be {1}..'.format(stack_name, 'update' if should_update else 'create')
+    try:
+        waiter.wait(StackName=stack_name)
+    except botocore.exceptions.ClientError as err:
+        print 'Failed to deploy stack {}'.format(stack_name)
+        print err
+    print 'Stack request complete!'
+    print 'Retrieving '
+
+def upload_journal_views(apigateway_client, api_gateway_identifier, region):
+    all_apis = apigateway_client.get_rest_apis(
+        limit=500
+    )
+
+    for api in all_apis['items']:
+        if api['name'] == api_gateway_identifier:
+            deployed_api_url = 'https://{0}.execute-api.{1}.amazonaws.com/{2}/'.format(api.id, region, 'dev')
+            # TODO: Update built view index with
+
+    raise RuntimeError('Failed to find an API Gateway matching the name {}. Unable to upload views.'.format(api_gateway_identifier))
