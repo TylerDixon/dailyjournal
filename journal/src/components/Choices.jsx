3cba90aff43b30d1b9ba69bb9422fcc48d7b09d3
Tyler Dixon <TylerD1242@gmail.com>
2017-02-19 16:31:21 -0500

    Update styles (#5)
    * Updating styles on form components
    
    * Adding simple styles to page/components
    * Fixing issue with updating checkbox states
    
    * Updating favicon from React default to custom favicon

--- journal/src/components/Choices.jsx
+++ journal/src/components/Choices.jsx
@@ -10,24 +10,20 @@ class Choices extends Component {
         this.isSingleSelect = this.props.elementConfig.type === 'radio'
     }
     toggleCheckbox(event) {
-        const currentValue = this.props.value;
+        var currentValue = this.props.value.slice(0);
         const currentIndex = currentValue.indexOf(event.target.value);
         if(currentIndex > -1){
-            this.props.onChange({
-                target: {
-                    id: event.target.name,
-                    value: currentValue.slice(0).splice(currentIndex, 1)
-                }
-            });
+            currentValue.splice(currentIndex, 1);
         }
         else {
-            this.props.onChange({
-                target: {
-                    id: event.target.name,
-                    value: currentValue.concat([event.target.value])
-                }
-            });
+            currentValue = currentValue.concat([event.target.value]);
         }
+        this.props.onChange({
+            target: {
+                id: event.target.name,
+                value: currentValue
+            }
+        });
     }
     isCheckboxChecked(choice) {
         return this.props.value.indexOf(choice) > -1
@@ -47,12 +43,12 @@ class Choices extends Component {
 
     render() {
         var renderedChoices = this.props.elementConfig.choices.map(choice => {
-            return <span><input value={choice} onChange={this.isSingleSelect ? this.toggleRadio.bind(this) : this.toggleCheckbox.bind(this)}
+            return <label className="input-container"><input value={choice} onChange={this.isSingleSelect ? this.toggleRadio.bind(this) : this.toggleCheckbox.bind(this)}
                           key={this.props.elementConfig.id + choice}
                    name={this.props.elementConfig.id}
                    id={this.props.elementConfig.id}
                    checked={this.isSingleSelect ? this.isRadioChecked(choice) : this.isCheckboxChecked(choice)}
-                   type={this.props.elementConfig.type} />{choice}</span>
+                   type={this.props.elementConfig.type} />{choice}</label>
         });
         return <span>{renderedChoices}</span>;
     }
